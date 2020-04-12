a1dc3decc737e59846f0b61cb2b1b6d6a9fd6999
Ana Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-04-12 07:10:41 -0500

    completed secondary database migration

--- part_django/part/models.py
+++ part_django/part/models.py
@@ -7,3 +7,12 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+class Item_part(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='item_parts')
+    part_name = models.CharField(max_length=100)
+    part_number = models.CharField(max_length=100) 
+    part_notes = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
